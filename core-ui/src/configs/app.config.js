6dc3b6a491bb375c1de1d564a859a3a695110c5e
Jhantu Nandi <jhantunandi4@gmail.com>
2025-03-13 20:22:06 +0530

    added port support

--- core-ui/src/configs/app.config.js
+++ core-ui/src/configs/app.config.js
@@ -1,7 +1,7 @@
 const config = Object.freeze({
     APP: {
         APP_URL: "https://crispy-pancake-5v6vgg4jr65hvj9p-3233.app.github.dev",
-        PORT: "" || 3333,
+        PORT: 3233,
     },
 
     DTATABASE: {
